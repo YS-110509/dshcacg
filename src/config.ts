185c171afc0ef590bc9ef50829e99647827d9f4d
桜島芦花 <2035566353@qq.com>
2026-05-01 01:26:26 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "大上海萌资讯",
 	subtitle: "郑州大上海城二次元交流群官网",
-	siteURL: "https://dshcacg.fun/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://www.dshcacg.fun/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-08-20", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
