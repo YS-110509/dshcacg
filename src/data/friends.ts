92448776a8eaca1a71065e8f8f0e91ae01c14fe7
桜島芦花 <2035566353@qq.com>
2026-05-01 22:57:39 +0800

    Update friends.ts

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -15,7 +15,7 @@ export const friendsData: FriendItem[] = [
 	{
 		id: 1,
 		title: "大上海萌资讯创作服务",
-		imgurl: "/_image/?href=%2F%40fs%2FC%3A%2FUsers%2F%E8%80%81%E8%83%A5%2FDesktop%2FSiroukin+Team%2F%E5%A4%A7%E4%B8%8A%E6%B5%B7%E8%90%8C%E8%B5%84%E8%AE%AF%2Fsrc%2Fassets%2Fimages%2Favatar.webp%3ForigWidth%3D948%26origHeight%3D948%26origFormat%3Dpng&w=948&h=948&f=webp",
+		imgurl: "https://dshcacg.fun/assets/home/home.webp",
 		desc: "向【大上海萌资讯】投稿文章、发表视频、违规举报、申请友链等。",
 		siteurl: "https://pd.qq.com/s/5r7k9asff",
 		tags: ["友链申请", "发表创作"],
